1dc427c6ae404245905349b9d1183b21e6cde4cb
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-06-06 11:29:43 -0300

    Segundo commit: ejer 1, calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,34 @@
+
+namespace EspacioCalculadora;
+
+public class Calculadora
+{
+   
+    private double dato = 0;
+   
+    public double Resultado
+    { 
+        get => dato; 
+
+    } 
+    
+    public void Sumar(double termino){
+        dato += termino; 
+    }
+    public void Restar(double termino){
+        dato -= termino; 
+    }
+
+    public void Multiplicar(double termino){
+        dato *= termino;    
+    }
+
+    public void Dividir(double termino){
+        dato /= termino;
+    }
+
+    public void Limpiar(){
+        dato = 0;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EspacioCalculadora;
+
+Calculadora miCalculadora = new Calculadora();
+
+bool anda = false;
+string? numString = "";
+int num=0, operacion = 0;
+string? operar = "S";
+
+while (!anda) {
+    Console.WriteLine("Ingrese un número: ");
+    numString = Console.ReadLine();
+    anda = int.TryParse(numString, out num);
+    if (!anda) {
+        Console.WriteLine(numString + " no es numero válido.");   
+    }
+}
+
+anda = false;
+
+while (!anda) {
+    Console.WriteLine("Ingrese la operacion que desea realizar: (1:SUMAR 2:RESTAR 3:MULTIPLICAR 4:DIVIDIR)");
+    numString = Console.ReadLine();
+    anda = int.TryParse(numString, out operacion);
+    //Control de numero
+    if (!anda) {
+        Console.WriteLine(numString + " no es numero válido.");   
+    }
+    if (operacion > 4 ){
+        anda = false;
+        Console.WriteLine(operacion + " no es una opción válida.");
+    }
+}
+
+
+anda = false;
+
+while (operar == "S"){
+
+    switch (operacion)
+    {
+        case 1: 
+                miCalculadora.Sumar(num);
+                Console.WriteLine( miCalculadora.Resultado );
+            break;
+
+        case 2: 
+                miCalculadora.Restar(num);
+                Console.WriteLine( miCalculadora.Resultado );
+            break;
+
+        case 3:
+                miCalculadora.Multiplicar(num);
+                Console.WriteLine( miCalculadora.Resultado );
+            break;
+
+        case 4:
+                if (num == 0)
+                {
+                    while (!anda) {
+                        Console.WriteLine("Ingrese un número: ");
+                        numString = Console.ReadLine();
+                        anda = int.TryParse(numString, out num);
+                        if (!anda) {
+                            Console.WriteLine(numString + " no es numero válido.");   
+                        }
+                        if (num == 0)
+                        {
+                            anda = false;
+                        }
+                    }
+                }
+                miCalculadora.Dividir(num);
+                Console.WriteLine( miCalculadora.Resultado );
+            break;    
+
+    }
+
+  
+    anda = false;
+    //Control para continuar operando
+    while (!anda)
+    {
+        Console.WriteLine("Seguir operando: S (SI), N (NO)");
+        operar = Console.ReadLine();
+        if (operar != "S" && operar != "N") {
+            anda = false;
+        }else
+        {
+          anda=true; 
+        }
+    } 
+
+    if (operar == "N")
+    {
+        miCalculadora.Limpiar();
+    }
+
+}
+
+        
+   
