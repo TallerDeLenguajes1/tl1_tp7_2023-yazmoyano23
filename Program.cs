e23a87d6808e18b9626bf780ba52565d2edfc3f6
yazmoyano <moyanoyazminalejandra@gmail.com>
2023-06-29 01:42:58 -0300

    Se agregaron controles en las operaciones

--- Program.cs
+++ Program.cs
@@ -42,17 +42,17 @@ while (operar == "S"){
     {
         case 1: 
                 miCalculadora.Sumar(num);
-                Console.WriteLine( miCalculadora.Resultado );
+                Console.WriteLine( "Resultado de la suma: " + miCalculadora.Resultado );
             break;
 
         case 2: 
                 miCalculadora.Restar(num);
-                Console.WriteLine( miCalculadora.Resultado );
+                Console.WriteLine( "Resultado de la resta: " + miCalculadora.Resultado );
             break;
 
         case 3:
                 miCalculadora.Multiplicar(num);
-                Console.WriteLine( miCalculadora.Resultado );
+                Console.WriteLine( "Resultado de la multiplicación: " + miCalculadora.Resultado );
             break;
 
         case 4:
@@ -73,7 +73,10 @@ while (operar == "S"){
                 }
                 miCalculadora.Dividir(num);
                 Console.WriteLine( miCalculadora.Resultado );
-            break;    
+            break;
+        case 5:
+                miCalculadora.Limpiar();
+            break;        
 
     }
 
@@ -92,9 +95,38 @@ while (operar == "S"){
         }
     } 
 
+    anda = false;
+
     if (operar == "N")
     {
+        Console.WriteLine("\nEl resultado final es: " + miCalculadora.Resultado);
         miCalculadora.Limpiar();
+    } else {
+        while (!anda) {
+            Console.WriteLine("Ingrese un número: ");
+            numString = Console.ReadLine();
+            anda = int.TryParse(numString, out num);
+            if (!anda) {
+                Console.WriteLine(numString + " no es numero válido.");   
+            }
+        }
+    }
+
+        anda = false;
+
+    //Control para realizar una nueva operacion
+    while (!anda) {
+        Console.WriteLine("Ingrese la operacion que desea realizar: (1:SUMAR 2:RESTAR 3:MULTIPLICAR 4:DIVIDIR)");
+        numString = Console.ReadLine();
+        anda = int.TryParse(numString, out operacion);
+        //Control de numero
+        if (!anda) {
+            Console.WriteLine(numString + " no es numero válido.");   
+        }
+        if (operacion > 4 ){
+            anda = false;
+            Console.WriteLine(operacion + " no es una opción válida.");
+        }
     }
 
 }
